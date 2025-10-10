48b09f96ab861fd5cb9173786e3b43637aaca921
Dks2006-crypto <denis18kpina@gmail.com>
2025-10-10 17:58:32 +0300

    commit3

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,13 @@
 import {LoginForm} from "@/entities/auth";
+import {Container} from "@/components/ui/container";
 
 export default  function  LoginPage(){
     return (
         <div className="h-screen flex items-center justify-center">
-            <LoginForm className='max-w-[340px]'/>
+            <Container className="w-full">
+                <LoginForm className='max-w-full'/>
+            </Container>
+
 
         </div>
     );
--- src/entities/auth/ui/login-form.ui.tsx
+++ src/entities/auth/ui/login-form.ui.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import {Label} from "@/components/ui/label";
+import Image from "next/image";
+
+import LoginBg from '../../../../public/statics/gradient.png'
 
 interface Props {
     className?: string;
@@ -7,11 +10,11 @@ interface Props {
 
 export const loginFormUi:React.FC <Props> = ({className}) => {
     return(
-        <div className={` ${className} p-5 w-full bg-gray-950 rounded-lg`}>
-            <div className="">
-
+        <div className={` ${className} flex w-full items-center overflow-hidden bg-gray-800 rounded-lg`}>
+            <div id='form-bg' className=" flex-1">
+                <Image src={LoginBg} alt={'Авторизация'} />
             </div>
-            <form className='w-full max-w-[340px] grid gap-y-4'>
+            <form className='p-5 flex-1 grid gap-y-4'>
                 <div className='w-full grid gap-y-2' id='form-group-1'>
                     <Label htmlFor='email'>E-mail</Label>
                     <input type='email' id='email' className='h-12 px-3.5 border rounded-full border-zinc-600 ' placeholder='aaaa.ry'/>
@@ -19,6 +22,9 @@ export const loginFormUi:React.FC <Props> = ({className}) => {
                 <div className='w-full grid gap-y-2' id='form-group-2'>
                     <Label htmlFor='password'>Password</Label>
                     <input type='password' id='password' className='h-12 px-3.5 border rounded-full border-zinc-600 ' placeholder='password'/>
+                </div>
+                <div className="w-full grid gap-y-2" id='form-group-3'>
+                    <button className="bg-gray-100 font-medium text-zinc-500 px-3 h-12 rounded-full cursor-pointer">Войти</button>
                 </div>
             </form>
         </div>
